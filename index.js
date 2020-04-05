d0b16c204f361dc993cd1ee2665e759630ba5c87
JeffreyDavidsz <jeffrey.davidsz@vicreo.eu>
2020-04-05 23:09:21 +0200

    some rewrite for module

--- index.js
+++ index.js
@@ -39,38 +39,32 @@ instance.prototype.config_fields = function () {
 			id: 'info',
 			width: 12,
 			label: 'Information',
-			value: 'http://x.x.x.x<br>[port]'
+			value: 'Connect to BTT with the IP and port, see help for more Information'
 		},
 		{
 			type: 'textinput',
-			id: 'prefix',
-			label: 'BTT URL',
-			width: 12
+			id: 'host',
+			label: 'BTT IP address',
+			width: 12,
+			regex: self.REGEX_IP
 		},
 		{
 			type: 'textinput',
 			id: 'port',
 			label: 'port',
-			width: 12
+			width: 12,
+			regex: self.REGEX_PORT
 		}
 	]
 }
 
 // When module gets deleted
 instance.prototype.destroy = function() {
-	var self = this;
 	debug("destroy");
 }
 
 instance.prototype.actions = function(system) {
 	var self = this;
-	var urlLabel = 'UUID';
-
-	if ( self.config.prefix !== undefined ) {
-		if ( self.config.prefix.length > 0 ) {
-			urlLabel = 'UUID';
-		}
-	}
 
 	self.setActions({
 		'uuid': {
@@ -78,8 +72,8 @@ instance.prototype.actions = function(system) {
 			options: [
 				{
 					type: 'textinput',
-					label: urlLabel,
-					id: 'uuid_url',
+					label: 'The UUID',
+					id: 'uuid',
 					default: '',
 				}
 			]
@@ -91,19 +85,12 @@ instance.prototype.action = function(action) {
 	var self = this;
 	var cmd;
 
-	if ( self.config.prefix !== undefined && action.options.uuid_url.substring(0,4) != 'http' ) {
-		if ( self.config.prefix.length > 0 ) {
-			cmd = self.config.prefix + ':'+ self.config.port + '/execute_assigned_actions_for_trigger/?uuid='	+	action.options.uuid_url;
-		}
-		else {
-			cmd = action.options.uuid_url;
-		}
-	}
-	else {
-		cmd = action.options.uuid_url;
-	}
+
+
 
 if (action.action == 'uuid') {
+
+		cmd = `http://${self.config.host}:${self.config.port}/execute_assigned_actions_for_trigger/?uuid=${action.options.uuid}`;
 
 		self.system.emit('rest_get', cmd, function (err, result) {
 			if (err !== null) {
